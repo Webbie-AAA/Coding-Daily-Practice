40c5a3d2a27c32d959f033fa893ac1c97b06488b
Webbie-AAA <umelbaneenalmousawi@gmail.com>
2025-12-26 19:19:30 +0000

    Complete r_p_s testing

--- day3/main.py
+++ day3/main.py
@@ -9,23 +9,18 @@ import random
 
 
 def computer():
-    choice = random.randint(0, 2)
-    return choice
+    return random.randint(0, 2)
 
 
-def user_play():
-    player_choice = int(
-        input("\nType 0 for Rock, 1 for Paper, and 2 for Scissors:\t"))
+def user_play(player_choice):
     if player_choice in [0, 1, 2]:
         print(f"You chose {player_choice}")
-        return rock_paper_scissors(player_choice)
+        return player_choice
     else:
-        print("Invalid input. Please try again")
+        raise ValueError("Invalid input. Type an integer in [0,1,2]")
 
 
-def rock_paper_scissors(user_choice: int) -> str:
-    computer_choice = computer()
-    user_choice = user_play()
+def rock_paper_scissors(user_choice: int, computer_choice: int) -> str:
     if user_choice == computer_choice:
         return "\nThe computer chose: " + str(computer_choice) + "\nIt's a draw"
     elif user_choice == 0 and computer_choice == 2:
@@ -38,15 +33,26 @@ def rock_paper_scissors(user_choice: int) -> str:
         return "\nThe computer chose: " + str(computer_choice) + "\nYou loose"
 
 
-game_play = True
-while game_play:
-    user_choice = user_play()
-    try:
-        print(rock_paper_scissors(user_choice))
-    except ValueError:
-        print("Invalid input. Please choose an int value of either 0,1, or 2.")
-        rock_paper_scissors(user_choice)
-    play_game = input(
-        "\nWould you like to play another round of 'Rock, Paper, Scissors'. If yes type 'y', if not type 'n':\t").lower().strip()
-    if play_game == "n":
-        game_play = False
+if __name__ == "__main__":
+    game_play = True
+    while game_play:
+        user_choice = int(
+            input("\nType 0 for Rock, 1 for Paper, and 2 for Scissors:\t"))
+        computer_choice = computer()
+        try:
+            print(rock_paper_scissors(user_choice, computer_choice))
+        except ValueError:
+            print("Invalid input. Please choose an int value of either 0,1, or 2.")
+            rock_paper_scissors(user_choice)
+        play_game = input(
+            "\nWould you like to play another round of 'Rock, Paper, Scissors'. If yes type 'y', if not type 'n':\t").lower().strip()
+        if play_game == "n":
+            game_play = False
+
+    # You must always kee "game logic" separate from input and output.
+    # Separate the parts that depend on randomness from use inputs so that it's testable (logic separate from input separate from output)
+    # You must not let functions mix concerns so each function must be isolated
+    # Logic specific functions must NOT ask for inputs and print
+    # Logic never talks directly to input and output
+    # Keep functions small and testable
+    # Separation of concerns
--- day3/test.py
+++ day3/test.py
@@ -1,25 +1,60 @@
 import pytest
-from main import rock_paper_scissors, computer
+from main import rock_paper_scissors, user_play
 
 
-# def test_rock_paper_scissors_draw():
-#     person_choice = 0
-#     computer() == 0
-#     assert rock_paper_scissors(person_choice) == "It's a draw"
+def test_rock_paper_scissors_draw():
+    # Arrange
+    user_choice = 0
+    computer_choice = 0
+    # Assert
+    assert rock_paper_scissors(
+        user_choice, computer_choice) == "\nThe computer chose: " + str(computer_choice) + "\nIt's a draw"
 
 
-# def test_rock_paper_scissors_rock_beats_scissors():
-#     assert rock_paper_scissors
-#     ...
+def test_rock_paper_scissors_rock_beats_scissors():
+    # Arrange
+    user_choice = 0
+    computer_choice = 2
+    # Act
+    result = rock_paper_scissors(user_choice, computer_choice)
+    # Assert
+    assert result == "\nThe computer chose: " + \
+        str(computer_choice) + "\nYou win"
 
 
-# def test_rock_paper_scissors_paper_beats_rock():
-#     ...
+def test_rock_paper_scissors_paper_beats_rock():
+    # Arrange
+    user_choice = 1
+    computer_choice = 0
+    # Act
+    result = rock_paper_scissors(user_choice, computer_choice)
+    # Assert
+    assert result == "\nThe computer chose: " + \
+        str(computer_choice) + "\nYou win"
 
 
-# def test_rock_paper_scissors_scissors_beats_paper():
-#     ...
+def test_rock_paper_scissors_scissors_beats_paper():
+    # Arrange
+    user_choice = 2
+    computer_choice = 1
+    # Act
+    result = rock_paper_scissors(user_choice, computer_choice)
+    # Assert
+    assert result == "\nThe computer chose: " + \
+        str(computer_choice) + "\nYou win"
 
 
-# def test_rock_paper_scissors_rock_beats_scissors():
-#     ...
+def test_rock_paper_scissors_scissors_loses_to_paper():
+    # Arrange
+    user_choice = 1
+    computer_choice = 2
+    # Act
+    result = rock_paper_scissors(user_choice, computer_choice)
+    # Assert
+    assert result == "\nThe computer chose: " + \
+        str(computer_choice) + "\nYou loose"
+
+
+def test_user_choice_input_error():
+    with pytest.raises(ValueError):
+        user_play(4)
